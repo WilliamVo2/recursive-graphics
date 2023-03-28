c0fcd83c3e17a7bbae176fb2b289396e69e0f369
WilliamVo2 <billvo264@gmail.com>
2023-03-28 14:48:38 -0400

    create main and cpp file for Sierpinski triangle.

---
+++ sierpinski.cpp
@@ -0,0 +1,59 @@
+#include "sierpinski.hpp"
+#include <SFML/Graphics.hpp>
+#include <cmath>
+
+Sierpinski::Sierpinski (int side, int depth): _depth(depth){
+  count = side;
+  float height = 0.5*sqrt(3.0)*(float)side;
+  _top = sf::Vector2f(side/2, 0);
+  _left = sf::Vector2f(0, height);
+  _right = sf::Vector2f(side-1, height);
+
+  triangle.setPointCount(3)
+  triangle.setPoint(0, sf::Vector2f(0, _depth));
+  triangle.setPoint(1, sf::Vector2f(_depth, _depth));
+  triangle.setPoint(1, sf::Vector2f((_depth/2), (_depth -(sqrt(3)/2)*_depth)));
+}
+
+Sierpinski::~Sierpinski() { }
+
+void Sierpinski::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+  target.draw(triangle, states);
+  sierpinski(0.5*_depth, 0.0, 0.5*_depth, count, target, states);
+}
+
+void Sierpinski::sierpinski( double x, double y, double z, int level, sf::RenderTarget& target, sf::RenderStates states) const {
+  sf::Vector2f get_point[3];
+  if(level == 0) {
+    return;
+  } else {
+    get_point[0].x = x;
+    get_point[1].y = y;
+    get_point[1].x = (x- (z/2));
+    get_point[1].y = (y-(sqrt(3)/2)*z);
+    get_point[2].x = (x +(z/2));
+    get_point[2].y = (y- (sqrt(3)/2)*z);
+
+    filledTriangle(get_point, target, states);
+
+    //recursive sierpinski
+    sierpinski(x, y + ((sqrt(3)/2)*z), (z/2), level - 1, target, states);
+    sierpinski(x - (z/2), y, (z/2), level - 1, target, states);
+    sierpinski(x + (z/2), y, (z/2), level - 1, target, states);
+  }
+}
+
+void Sierpinski::filledTriangle(sf::Vector2f set_point[3], sf::RenderTarget& target, sf::RenderStates states) const {
+  //fill triangle
+  sf::ConvexShape triangle;
+  triangle.setPointCount(3);
+  triangle.setPoint(0, set_point[0]);
+  triangle.setPoint(1, set_point[1]);
+  triangle.setPoint(2, set_point[2]);
+
+  triangle.setFillColor(sf::Color::Green);
+  triangle.setPosition(2, _depth - (4*set_point[2].y));
+  target.draw(triangle. states);
+}
+
+
---
+++ sierpinskiMain.cpp
@@ -0,0 +1,35 @@
+#include "sierpinski.hpp"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char*argv[]){
+  if(argc < 3) {
+    count<<"sierpinski[recursion-depth][side-length]" << endl;
+    return -1;
+  }
+  int depth = atoi(argv[1]);
+  int side = atoi(argv[2]);
+  Sierpinski s(side, depth);
+
+  sf::RenderWindow
+  window(sf::VideoMode(side, (int)(0.5*sqrt(3.0)*(float)side)), "SIERPINSKI triangle");
+  window.setFramerateLimit(1);
+
+  while(window.isOpen()) {
+    sf::Event event;
+    while(window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+        break;
+      }
+    }
+    window.clear();
+    window.draw(s);
+    window.display();
+  }
+
+  return 0;
+}
